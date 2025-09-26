2e34787f485fe70cb5023f05bd401dfcbb6fa36c
rimiJI <hlji619@naver.com>
2025-09-26 16:45:15 +0900

    -

---
+++ 01-q3-algorithm.js
@@ -0,0 +1,22 @@
+// Q3 0들을 맨 뒤로 빼기. 복사말고 변경(in place로)
+let moveZeroToBack = function () {};
+/**
+You are working on a to-do list app. The tasks are stored in an integer array nums, where each number represents a task ID.
+
+Sometimes, unfinished tasks are marked as 0.
+Before showing the list to the user, the app wants to move all the unfinished tasks (0s) to the bottom, while keeping the order of the other tasks the same.
+
+Important: You must do this in-place (without making a new copy of the list).
+
+Example 1:
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+Example 2:
+Input: nums = [0]
+Output: [0]
+
+Example 3:
+Input: nums = [1,0,2,0,3,0]
+Output: [1,2,3,0,0,0]
+ */
