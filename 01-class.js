549475159de44534700e1a9a899f609ffb97c175
rimiJI <hlji619@naver.com>
2025-10-01 10:30:11 +0900

    -

--- 01-class.js
+++ 01-class.js
@@ -7,7 +7,7 @@ let a = [1, 2, 3, 4, 5, 6];
 function sumEvenNumbers(arr) {
   let evensum = 0;
   for (let i = 0; i < arr.length; i++) {
-    if (a[i] % 2 == 0) evensum += a[i];
+    if (arr[i] % 2 == 0) evensum += arr[i];
   }
   return evensum;
 }
