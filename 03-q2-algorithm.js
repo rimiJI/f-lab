0c7980caeebba1359591f7af4cc62e1844d0b7bb
rimiJI <hlji619@naver.com>
2025-10-23 16:16:46 +0900

    -

--- 03-q2-algorithm.js
+++ 03-q2-algorithm.js
@@ -5,22 +5,27 @@ Sum Odd Numbers Deep
 
 /*
 방법1. 만약 원소가 어레이냐? 아니냐? 
-어레이다 - 시간과생각의 방으로 가 ( 재귀 ) 
+어레이다 - 시간과공간의 방으로 가 ( 재귀 ) 
 어레이 아님 && 홀수임 - 덧셈스칼라에 더하셈
-
 변수공간 : 덧셈스칼라 , 원소가어레이일 경우
+시간복잡도: O(n) 모든 원소를 한번씩만 돌기때문에
+공간복잡도: O(k) k는 oddSum이 반복해서 생겨난 횟수. 
+공간복잡도 이유: JS는 함수 호출시, 새 scope를 생성하고 그 안에 변수 저장하고 끝나면 pop💥 
+               그래서 다른 호출과 섞기지❌, 한 이름이 재사용❌, 계속 생겼다 없어지길 반복.
 */
 var sumOddNumbersDeep = function (nums) {
-  let sum = 0;
+  let oddSum = 0; //처음호출에만 존재
   for (i of nums)
     if (Array.isArray(i)) {
-      sum += sumOddNumbersDeep(i); //이렇게 새롭게 재귀 불러오면 기존 sum결과를 더해줘야함
+      oddSum += sumOddNumbersDeep(i);
+      //이렇게 새롭게 재귀 불러오면 기존 sum결과를 더해줘야함
+      //처음엔 그냥 sumOddNumbersDeep(i) 만 했었음
     } else {
       if (i % 2 !== 0) {
-        sum += i;
+        oddSum += i;
       }
     }
-  return sum;
+  return oddSum;
 };
 
 console.log(sumOddNumbersDeep([1, [2, 3], [4, [5, 6]]]));
