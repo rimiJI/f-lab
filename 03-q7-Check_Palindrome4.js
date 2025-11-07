8e1476b787dc578c07a7955f8d6f3523a2964f8c
Hyerim JI <rimirimijj@gmail.com>
2025-11-07 23:50:36 +0900

    [251107]

--- 03-q7-Check_Palindrome4.js
+++ 03-q7-Check_Palindrome4.js
@@ -1,13 +1,13 @@
 /**
  * Check Palindrome4
- * 
+ *
  * 또 팔린드롬이에요. 앞으로 읽어도 뒤로 읽어도 같은 단어나 문장.
- * 
- * 문자열 's'가 주어지구요, 대신 재배열 해야 되요. 
- * 
+ *
+ * 문자열 's'가 주어지구요, 대신 재배열 해야 되요.
+ *
  * 재배열해서 팔린드롬을 만들수 있다. true
  * 없다. false
- * 
+ *
  * Example 1:
  * Input:  s = "civic"
  * Output: true
@@ -19,7 +19,7 @@
  * Example 3:
  * Input:  s = "hello"
  * Output: false
- * 
+ *
  * Example 4:
  * Input:  s = "aaaabbbb"
  * Output: true
@@ -39,7 +39,7 @@
  * Input:  s = "aabbccdde"
  * Output: false
  * Explanation: 홀수 개 문자가 2개 이상 → 불가능.
- * 
+ *
  * ----------------------------
  * Constraints (제한):
  * 1 <= s.length <= 1000
@@ -52,13 +52,27 @@ map으로 풀어서
 있으면 +- 이렇게 상쇄
 
 */
-var canFormPalindrome = function(s) {
+var isPalindrome = function (s) {
   // TODO
-  
+  //문자 하나씩 돌리면서 map에 set하고
+  let freqMap = new Map();
+  for (let i = 0; i < s.length; i++) {
+    let preValue = freqMap.get(s[i]) || 0;
+    freqMap.set(s[i], preValue + 1);
+  }
+
+  //odd가 하나일 경우 true
+  let odd = 0;
+  for (let [k, v] of freqMap) {
+    //짝수가 아니면 odd에 추가
+    if (v % 2 !== 0) odd++;
+    if (odd > 1) return false;
+  } //짝수면 탈출
+  return true;
 };
 
 console.log(isPalindrome("civic")); // true
 console.log(isPalindrome("ivicc")); // true
 console.log(isPalindrome("a")); // true
 console.log(isPalindrome("hello")); // false
-console.log(isPalindrome("aabbccdde")); // false 
+console.log(isPalindrome("aabbccdde")); // false
