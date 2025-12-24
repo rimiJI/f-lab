0d5bc12bd93cf147bc5d5dfa3e1c7e98df6b9f91
rimiJI <hlji619@naver.com>
2025-12-24 16:27:59 +0900

    이해하기

--- 29-Linked_List_5.js
+++ 29-Linked_List_5.js
@@ -42,7 +42,7 @@ function hasCycle(head) {
   let slow = head;
   let fast = head;
 
-  while (fast) {
+  while (fast && fast.next) {
     slow = slow.next;
     fast = fast.next.next;
     if (slow === fast) {
